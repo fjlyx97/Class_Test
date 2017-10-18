efe9ab7604b8831a0c4c41be290e12df70b52afc
fjlyx97 <541156120@qq.com>
2017-10-18 17:14:02 +0800

    add oj

---
+++ C_oj/1227.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    int T;
+    scanf("%d",&T);
+    double num[T][6];
+    int i = 0;
+    int j = 0;
+    double hp1_over = 0;
+    double hp2_over = 0;
+    for (j = 0 ; j < T ; j++)
+    {
+        for (i = 0 ; i < 6 ; i++)
+        {
+            scanf("%lf",&num[j][i]);
+        }
+    }
+    for (j = 0 ; j < T ; j++)
+    {
+        //hp1_over = num[j][0] / num[j][4] * num[j][5]; 
+        //hp2_over = num[j][3] / num[j][1] * num[j][2];
+        //if (hp1_over > hp2_over)
+        //{
+        //    printf("Yes\n");
+        //}
+        //else
+        //{
+        //    printf("No\n");
+        //}
+        while (num[j][0] > 0)
+        {
+            num[j][0] -= num[j][4];
+            hp1_over += num[j][5];
+        }
+        while (num[j][3] > 0)
+        {
+            num[j][3] -= num[j][1];
+            hp2_over += num[j][2];
+        }
+        if (hp1_over > hp2_over)
+        {
+            printf("Yes\n");
+        }
+        else
+        {
+            printf("No\n");
+        }
+    }
+    system("pause");
+    return 0;
+}
