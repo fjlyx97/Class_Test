f00c0f26a2e31c4942f6c702bb784aafcdd924ca
fjlyx97 <fjly97@outllok.com>
2019-01-04 17:26:38 +0800

    update

--- test/test.c
+++ test/test.c
@@ -1,88 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-
-void dump(int *arr, int size)
-{
-	int idx;
-
-	for (idx = 0; idx < size; idx++)
-		printf("%08d\n", arr[idx]);
-}
-
-void __merge(int *arr, int p, int q, int r)
-{
-	int *tmp;
-	int i, j, k;
-
-	tmp = (int*)malloc((r - p + 1) * sizeof(int));
-
-	if (!tmp)
-		abort();
-
-	for (i = p, j = q + 1, k = 0; i <= q && j <= r;) {
-		if (arr[i] <= arr[j])
-			tmp[k++] = arr[i++];
-		else
-			tmp[k++] = arr[j++];
-	}
-
-	if (i == q + 1) {
-		for (; j <= r;)
-			tmp[k++] = arr[j++];
-	} else {
-		for (; i <= q;)
-			tmp[k++] = arr[i++];
-	}
-
-	memcpy(arr + p, tmp, (r - p + 1) * sizeof(int));
-	free(tmp);
-}
-
-void __merge_sort(int *arr, int p, int r)
-{
-	int q;
-
-	if (p >= r)
-		return;
-
-	q = (p + r) / 2;
-	__merge_sort(arr, p, q);
-	__merge_sort(arr, q + 1, r);
-	__merge(arr, p, q, r);
-}
-
-void merge_sort(int *arr, int size)
-{
-	__merge_sort(arr, 0, size - 1);
-}
-
-void merge_verify()
-{
-	int test[10] = {5, 8, 9, 23, 67, 1, 3, 7, 31, 56};
-
-	__merge(test, 0, 4, 9);
-
-	dump(test, 10);
-}
-
-void merge_sort_test()
-{
-	int test[1] = {1};
-
-	merge_sort(test, 10);
-
-	dump(test, 10);
-}
-
+#include <math.h>
 int main()
 {
-	//merge_verify();
-	//merge_sort_test();
-	int a = 1;
-	int b = 4 , c = 3;
-	printf("%d",!(a<b) || !c && 1);
-	system("pause");
-	return 0;
+    int N;
+    int j = 0;
+    int o = 0;
+    int a;
+    scanf("%d",&N);
+    N = abs(N);
+    while(N)
+    {
+        a = N%10;
+        a = a%2;
+        N /= 10;
+        if (a == 1)
+            j++;
+        if (a == 0)
+            o++;
+    }
+    printf("%d,%d\n",j,o);
+    system("pause");
+    return 0;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,40 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+struct stack
+{
+	int top;
+	int num[20];
+};
 int main()
 {
-	int i,j,n;
-	char a[100][70], b[50]="0.99999999999999999999";
-	scanf("%d", &n);
-	getchar();
-	for (i = 0; i < n; i++)
-	{
-		gets(a[i]);
-	}
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; a[i][j] != '\0'; j++)
-		{
-			if (a[i][j] == b[j])
-			{
-				if (j == 22)
-				{
-					printf("Yes\n");
-					break;
-				}
-				continue;
-			}
-			else if (a[i][j] > b[j])
-			{
-				printf("Yes\n");
-				break;
-			}
-			else
-			{
-				printf("No\n");
-				break;
-			}
-		}
-	}
-    system("pause");
+	int n = 10 , m;
+	m = ++n;
+	n++;
+	
+	system("pause");
 	return 0;
 }
