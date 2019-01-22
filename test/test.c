1c90413e5e5ddfb4752a07041c55632363962267
fjlyx97 <fjly97@outllok.com>
2019-01-23 00:54:33 +0800

    update

--- test/test.c
+++ test/test.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define MAXSIZE 50
+struct SqList
+{ 
+    int data[MAXSIZE]; // 用数组表示顺序表 
+    int length; // 线性表当前长度
+};
+
 int main()
 {
-    int N;
-    int j = 0;
-    int o = 0;
-    int a;
-    scanf("%d",&N);
-    N = abs(N);
-    while(N)
-    {
-        a = N%10;
-        a = a%2;
-        N /= 10;
-        if (a == 1)
-            j++;
-        if (a == 0)
-            o++;
-    }
-    printf("%d,%d\n",j,o);
-    system("pause");
-    return 0;
+
 }
