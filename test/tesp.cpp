7cadd0749938d371eee8f42a0f0f6e5152073e8f
fjlyx97 <fjly97@outllok.com>
2019-03-24 19:29:14 +0800

    update

---
+++ test/tesp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+template<typename ElemType>
+struct queue
+{
+    ElemType* num;
+    ElemType* front;
+    ElemType* tail;
+};
+int main()
+{
+
+    system("pause");
+    return 0;
+}
